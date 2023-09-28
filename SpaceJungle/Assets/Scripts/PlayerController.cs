ff77d48c55b23dbd6e75507374307a850e71dce9
Aurore-Be <124353787+Aurore-Be@users.noreply.github.com>
2023-09-28 14:31:56 +0200

    ouverture sas

--- SpaceJungle/Assets/Scripts/PlayerController.cs
+++ SpaceJungle/Assets/Scripts/PlayerController.cs
@@ -7,26 +7,63 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     [SerializeField] private float playerSpeed = 2.0f;
     [SerializeField] private float rotationSpeed = 2.0f;
+    bool canMove = false;
 
-    // Start is called before the first frame update
+    // CANVAS
+    [Header("Sas opening Info")]
+    [SerializeField] Canvas canvas;
+    [SerializeField] GameObject topBorder;
+    public int topBorderMax = 125;
+    public float speedOpening = 0.5f;
+
+    /// <summary>
+    /// Open sas
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator sasCoroutine()
+    {
+        float dist = 0;
+        while (dist < 0.45f)
+        {
+            topBorder.GetComponent<RectTransform>().position += new Vector3(0,
+                 Time.deltaTime * speedOpening, 0);
+            dist += Time.deltaTime * speedOpening;
+
+            yield return null;
+        };
+        yield return new WaitForSeconds(.5f);
+        canMove = true;
+    }
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
+
+        // Init sas opening
+        if (canvas == null)
+            Debug.Log("error : no canvas for player");
+        else
+        {
+            StartCoroutine(sasCoroutine());
+        }
     }
 
 
 
     void Update()
     {
-        float move = Input.GetAxis("Vertical");
-        float rotation = Input.GetAxis("Horizontal");
-
-        transform.Translate(Vector3.forward * move * playerSpeed * Time.deltaTime);
-
-        if (rotation != 0.0f)
+        if (canMove)
         {
-            //gameObject.transform.forward = move;
-            transform.Rotate(Vector3.up, rotation * rotationSpeed);
+            float move = Input.GetAxis("Vertical");
+            float rotation = Input.GetAxis("Horizontal");
+
+            transform.Translate(Vector3.forward * move * playerSpeed * Time.deltaTime);
+
+            if (rotation != 0.0f)
+            {
+                //gameObject.transform.forward = move;
+                transform.Rotate(Vector3.up, rotation * rotationSpeed);
+            }
         }
     }
 
