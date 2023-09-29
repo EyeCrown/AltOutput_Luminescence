da6567c06d016d7a69fe25693c021d08f8260ea9
Aurore-Be <124353787+Aurore-Be@users.noreply.github.com>
2023-09-29 15:46:47 +0200

    Update fade.cs

--- SpaceJungle/Assets/fade.cs
+++ SpaceJungle/Assets/fade.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class fade : MonoBehaviour
 {
+        bool flag = false;
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !flag)
         {
             collision.gameObject.GetComponent<PlayerController>().StartCoroutine(collision.gameObject.GetComponent<PlayerController>().sasCoroutineEnd());
+            flag = true;
         }
+
     }
 }
