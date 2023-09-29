253535a576d8352452d3bedb5464c6c3f3d39ae0
Aurore-Be <124353787+Aurore-Be@users.noreply.github.com>
2023-09-29 11:46:58 +0200

    creature + sons divers

--- SpaceJungle/Assets/CaveTrigger.cs
+++ SpaceJungle/Assets/CaveTrigger.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class CaveTrigger : MonoBehaviour
 {
     [SerializeField] bool enter;
+    [SerializeField] Animator creatureAnimator;
+    [SerializeField] AudioSource soundCreature;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             other.GetComponent<PlayerController>().isInCave = enter;
+            if (!enter)
+            {
+                creatureAnimator.SetTrigger("StartUp");
+                soundCreature.Play();
+            }
         }
     }
 }
